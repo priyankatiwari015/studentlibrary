f996aab810ed28a39a2d475402afd91c36cee9b3
priyankatiwari015 <68387063+priyankatiwari015@users.noreply.github.com>
2022-02-27 10:49:19 +0530

    Add files via upload
    Files of Nikhitha are also added

---
+++ LibraryManagementSystem/staff.php
@@ -0,0 +1,71 @@
+<?php
+include("setting.php");
+session_start();
+if(isset($_SESSION['stid']))
+{
+	header("location:home.php");
+}
+$sid=mysqli_real_escape_string($set,$_POST['stid']);
+$pass=mysqli_real_escape_string($set,$_POST['pass']);
+
+if($stid==NULL || $_POST['pass']==NULL)
+{
+	//
+}
+else
+{
+	$p=sha1($pass);
+	$sql=mysqli_query($set,"SELECT * FROM students WHERE sid='$stid' AND password='$pass'");
+	if(mysqli_num_rows($sql)==1)
+	{
+		$_SESSION['stid']=$_POST['stid'];
+		header("location:home.php");
+	}
+	else
+	{
+		$msg="Incorrect Details";
+	}
+}
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title> Student Library </title>
+<link href="stylesheet.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+<div id="banner">
+<span class="head">e-Library </span><br />
+</div>
+<br />
+
+<div align="center">
+<div id="wrapper">
+<br />
+<br />
+
+<span class="SubHead">Staff Sign In</span>
+<br />
+<br />
+<form method="post" action="">
+<table border="0" cellpadding="4" cellspacing="4" class="table">
+<tr><td colspan="2" align="center" class="msg"><?php echo $msg;?></td></tr>
+<tr><td class="labels">Staff ID : </td><td><input type="text" name="sid" class="fields" size="25" placeholder="Enter Student ID" required="required" /></td></tr>
+<tr><td class="labels">Password : </td><td><input type="password" name="pass" class="fields" size="25" placeholder="Enter Password" required="required" /></td></tr>
+<tr><td colspan="2" align="center"><input  type="submit" value="Sign In" class="fields" /></td></tr>
+</table>
+</form>
+<br />
+<br />
+<a href="index.html" class="link">Home page</a></br>
+<a href="staffpage.html">Staff page</a>
+
+<br />
+<br />
+
+</div>
+</div>
+</body>
+</html>
